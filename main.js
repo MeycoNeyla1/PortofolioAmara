5904e27c4a512e16f66c48f32a59093145221587
Amara <amara@example.com>
2026-06-27 20:03:39 +0700

    Remove encodeURI that might break image paths on Vercel

--- main.js
+++ main.js
@@ -179,7 +179,7 @@ const lightboxImg = document.getElementById('lightbox-img');
 const closeLightbox = document.querySelector('.close-lightbox');
 
 window.openLightbox = (src) => {
-  lightboxImg.src = encodeURI(src);
+  lightboxImg.src = src;
   lightbox.classList.add('show');
 }
 
